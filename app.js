c194a31095126a17223ea6f8b5525fed97a4114f
damp07 <157430071+damp07@users.noreply.github.com>
2024-06-08 22:28:38 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,45 +1,104 @@
-alert('Hola mundo') //El uso de comillas simples ('') o dobles (""), en JScript es indiferente, pero con la particularidad de usar las mismas en la línea de código
-//En JScript, el uso de ";" no es obligatorio
+/*
+let titulo = document.querySelector('h1'); //Este método nos ayuda a seleccionar elementos que están en el archivo index.html
+//titulo no es una variable sino un objeto
+let parrafo = document.querySelector('p');
 
-//Declaración de variables
-// let numeroSecreto = 6;
-let numeroSecreto = Math.floor(Math.random()*10) + 1; //Creación de un número pseudoaleatorio desde 1 hasta 10;
-let numeroUsuario = 0;
-let intentos = 1;
-let palabraVeces = 'vez'
-let maxIntentos = 3;
+titulo.innerHTML = 'Juego del número secreto';
+parrafo.innerHTML = 'Indica un número';
+*/
 
-while(numeroUsuario!=numeroSecreto){ //Comparación de desigualdad '!='
-    ;
-    numeroUsuario = parseInt(prompt("Me indicas un número entero entre 1 y 10 por favor:")); //Solicitando un número al usuario
-    console.log(numeroUsuario + ' ' + intentos); //Muestra la variable en la cónsola del navegador
-    console.log(numeroSecreto)
-    /*
-    Código de comparación
-    */
-    if (numeroUsuario == numeroSecreto) {
-        //Condición de acierto
-        // alert(`FELICIDADES! Acertaste con el número: ${numeroUsuario} en ${intentos} ${palabraVeces}`);
-        alert(`FELICIDADES! Acertaste con el número: ${numeroUsuario} en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+// function VerificarUsuario(){ //Declaración de la función 'intentoDeUsuario'
+//     // let numeroDeUsuario = document.querySelector('input').value; otra forma de obtener el valor
+//     let numeroDeUsuario = document.getElementById('valorUsuario').value;
+//     numeroDeUsuario = parseInt(numeroDeUsuario) //Convertir la variable a tipo 'número'
+//     console.log(numeroDeUsuario);
+//     console.log(typeof(numeroSecreto)); //Consulta del tipo de la variable con el 'typeof'
+//     console.log(numeroSecreto);
+//     console.log(typeof(numeroSecreto)); //Consulta del tipo de la variable con el 'typeof'
+//     console.log(numeroSecreto == numeroDeUsuario) //Verificación de la igualdad de ambas variables
+//     console.log(numeroSecreto === numeroDeUsuario) //verificación de la igualdad entre valor y tipo
+//     return;
+// }
 
-        // El uso de las comillas invertidas `` se realiza para las "templates string", en donde se adicionan variables en cadenas de texto
-        // La sintaxis ${} es usada para llamar una variable en cadena de texto
-    }else{
-        //Condición de fallo
-        if(numeroUsuario > numeroSecreto){
-            alert('El número secreto es menor');
-        } else alert('El número secreto es mayor');
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+let numeroMaximo = 10;
 
-        //alert('Lo siento, pero no acertaste el número, así que vuelve a intentarlo');
-        //numeroUsuario = prompt("Me indicas un número entero entre 1 y 10 por favor:");
-    }
+condicionesIniciales();
 
-    intentos ++;
-    palabraVeces = 'veces';
-
-    // Limitar el número de intentos
-    if(intentos > maxIntentos){
-        alert(`Has alcanzado la máxima cantidad de ${maxIntentos} intentos. ADIOS!`);
-        break; //este break rompe el ciclo WHILE cuando se cumpla la condición
-    }
+function asignarNombreElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento); //Este método nos ayuda a seleccionar elementos que están en el archivo index.html
+    elementoHTML.innerHTML = texto;
 }
+
+function VerificarUsuario(){ //Declaración de la función 'intentoDeUsuario'
+    // let numeroDeUsuario = document.querySelector('input').value; otra forma de obtener el valor
+    let numeroDeUsuario = document.getElementById('valorUsuario').value;
+    numeroDeUsuario = parseInt(numeroDeUsuario) //Convertir la variable a tipo 'número'
+    console.log (`Llevas ${intentos} intentos`)
+    //factorial (numeroDeUsuario);
+
+    if (numeroSecreto === numeroDeUsuario){
+        asignarNombreElemento('p', `¡Felicidades! Acertaste el número en ${intentos} ${(intentos == 1)? 'intento' : 'intentos'}`)
+        document.getElementById('reiniciar').removeAttribute('disabled');
+        document.querySelector('#botonIntentar').setAttribute('disabled' , 'true');
+
+    } else{
+        limpiarCaja();
+        intentos++;
+        if (numeroSecreto > numeroDeUsuario){
+            asignarNombreElemento('p' , 'El número secreto es mayor');
+        } else asignarNombreElemento('p' , 'El número secreto es menor')
+    }
+    return;
+}
+
+function condicionesIniciales() {
+    asignarNombreElemento('h1', 'Juego del número secreto');
+    asignarNombreElemento('p', `Indica un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    document.querySelector('#botonIntentar').removeAttribute('disabled'); //Habilitar el botón de intentos
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = ''; 
+    //Al usar queryselector por el id, se debe indicar el numeral (#) y así el programa reconoce la búsqueda como tal.
+}
+
+function reiniciarJuego() {
+    limpiarCaja();
+    condicionesIniciales();
+    document.querySelector('#reiniciar').setAttribute('disabled' , 'true');
+    
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado = (Math.floor(Math.random()*numeroMaximo) + 1);
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    if (listaNumerosSorteados.length == numeroMaximo){
+        asignarNombreElemento('p' , 'Juego finalizado. Agotaste todos los intentos posibles');
+        console.log("Se acabaron los intentos");
+    } else {
+        //Si el número generado está incluido en la lista
+        if (listaNumerosSorteados.includes(numeroGenerado)){ //Verifica si el número está en la lista
+            return generarNumeroSecreto(); //Recursividad de la función, en donde se llama a si misma.
+        }else{
+                listaNumerosSorteados.push(numeroGenerado); //Incluye el número generado al final de la lista
+                return numeroGenerado;
+            }
+    }
+    
+}
+
+/*function factorial (numero){
+    let X = numero ;
+    while(numero > 1){
+        X = X * (numero - 1)
+        numero --;
+    }
+    return console.log(X)
+}*/
+
